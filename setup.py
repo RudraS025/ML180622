9847847af8d1a573dab54ecdbd167b0fadfbe35b
RudraS025 <rudra.s.25@gmail.com>
2022-06-27 20:56:55 +0530

    Updated with -e .

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.2"
 AUTHOR="RUSH"
 DESCRIPTION="This is a first FSDS Nov batch Machine Learning Project"
-PACKAGES=["housing"]
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -20,14 +20,14 @@ def get_requirements_list()->List[str]:
     requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 
 )
