e809e6e14ad1172c49917cb9458bce27449d843a
RudraS025 <rudra.s.25@gmail.com>
2022-08-10 05:15:25 +0530

    html done

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ def main():
     try:
         config_path = os.path.join("config","config.yaml")
         pipeline = Pipeline(Configuartion(config_file_path=config_path))
-        pipeline.run_pipeline()
+        #pipeline.run_pipeline()
         pipeline.start()
         logging.info("main function execution completed.")
         # # data_validation_config = Configuartion().get_data_transformation_config()
